bdf6b36ced1339e9e0e984e8157920482f479209
Cat Gomez <catherinedgomez@gmail.com>
2020-03-18 18:48:00 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,28 @@
 class Owner
-  # code goes here
+
+  attr_accessor :name, :species, :human
+
+  @@all = []
+
+  def initialize(name, species)
+    @name = name.freeze # has a name and cannot be changed
+    @species = species # initializes with species set to human and cannot be changed
+  end
+
+  def species
+    @species = human
+  end
+
+  def self.all
+    @@all
+  end
+
+  def say_species
+    puts "I am a #{@species}." # can say its species
+  end
+
+  def owner_count
+    self.owner_count = self.owner.length
+  end
+
 end
